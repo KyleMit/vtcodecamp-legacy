5ba45819e521316b99e097e2c7a250d27f17944e
KyleMit <kylemit@gmail.com>
2019-03-15 18:39:44 -0400

    add material UI component library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import CalendarToday from '@material-ui/icons/CalendarToday'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <Typography variant="h3" gutterBottom>
+          About the Event
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          The upcoming 11th annual Vermont Code Camp will be held on Saturday, September 28, 2019 in Burlington, Vermont.
+        </Typography>
+
+
+        <Button variant="contained" color="primary">
+          Sign Up On Meetup
+        </Button>
+
+        <CalendarToday />
       </div>
     );
   }
